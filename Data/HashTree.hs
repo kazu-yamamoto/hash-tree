62737a10f0f94c622a8bed55cdcbee4cf6b9c4e3
Kazu Yamamoto <kazu@iij.ad.jp>
2017-10-05 13:59:09 +0900

    better fromList algo from nobsun.

--- Data/HashTree.hs
+++ Data/HashTree.hs
@@ -1,7 +1,11 @@
-module Data.HashTree where
+module Data.HashTree (
+    Tree
+  , singleton
+  , link
+  , fromList
+  ) where
 
-import Data.Bits
-import Data.List
+-- import Data.Bits
 
 data Tree a = Leaf a
             | Node (Tree a) (Tree a) deriving (Eq, Show)
@@ -9,22 +13,30 @@ data Tree a = Leaf a
 singleton :: a -> Tree a
 singleton = Leaf
 
-join :: Tree a -> Tree a -> Tree a
-join = Node
+link :: Tree a -> Tree a -> Tree a
+link = Node
 
+-- |
+--
+-- >>> fromList [0..6 :: Int]
+-- Node (Node (Node (Leaf 0) (Leaf 1)) (Node (Leaf 2) (Leaf 3))) (Node (Node (Leaf 4) (Leaf 5)) (Leaf 6))
 fromList :: [a] -> Tree a
-fromList xs = reduce $ snd $ foldl' add (0,[]) $ map singleton xs
-  where
-    add (i,ts) t = (i+1, merge (mergeCount i) (t:ts))
-    merge 0 ts        = ts
-    merge n (t1:t2:ts) = merge (n - 1) (join t2 t1:ts)
-    merge _ _ = error "merge"
-    reduce [t] = t
-    reduce (t1:t2:ts) = reduce (join t2 t1:ts)
-    reduce _ = error "reduce"
+fromList [] = error "No Element"
+fromList xs = buildup (map singleton xs)
 
+buildup :: [Tree a] -> Tree a
+buildup [t] = t
+buildup ts  = buildup (pairing ts)
+
+pairing :: [Tree a] -> [Tree a]
+pairing (t:u:vs) = link t u : pairing vs
+pairing ts       = ts
+
+----------------------------------------------------------------
+{-
 mergeCount :: Int -> Int
 mergeCount = countTrailingZeros . complement
 
 log2Int :: Int -> Int
 log2Int x = finiteBitSize x - 1 - countLeadingZeros x
+-}
