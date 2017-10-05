2ba4ec1cfb61b5c78e0363e6d3be8b6b33a8fe54
Kazu Yamamoto <kazu@iij.ad.jp>
2017-10-05 15:51:22 +0900

    defining MerkleTreeHash and TreeHash.

--- Data/HashTree.hs
+++ Data/HashTree.hs
@@ -1,36 +1,82 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.HashTree (
-    Tree
-  , singleton
-  , link
+    MerkleTreeHash
+  , Settings(..)
+  , defaultSettings
+  , HashTree
   , fromList
   ) where
 
+import Data.ByteArray (ByteArrayAccess)
+import qualified Data.ByteArray as BA
+import Crypto.Hash
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import Data.ByteString.Char8 ()
 -- import Data.Bits
 
-data Tree a = Leaf a
-            | Node (Tree a) (Tree a) deriving (Eq, Show)
+newtype MerkleTreeHash a = MerkleTreeHash (Digest a) deriving (Eq, Show)
 
-singleton :: a -> Tree a
-singleton = Leaf
+instance ByteArrayAccess (MerkleTreeHash a) where
+  length (MerkleTreeHash dst) = BA.length dst
+  withByteArray (MerkleTreeHash dst) = BA.withByteArray dst
 
-link :: Tree a -> Tree a -> Tree a
-link = Node
+data Settings inp ha = Settings {
+    hash0 :: MerkleTreeHash ha
+  , hash1 :: inp -> MerkleTreeHash ha
+  , hash2 :: MerkleTreeHash ha -> MerkleTreeHash ha -> MerkleTreeHash ha
+  }
 
--- |
---
--- >>> fromList [0..6 :: Int]
--- Node (Node (Node (Leaf 0) (Leaf 1)) (Node (Leaf 2) (Leaf 3))) (Node (Node (Leaf 4) (Leaf 5)) (Leaf 6))
-fromList :: [a] -> Tree a
-fromList [] = error "No Element"
-fromList xs = buildup (map singleton xs)
+sha256 :: ByteString -> Digest SHA256
+sha256 = hash
 
-buildup :: [Tree a] -> Tree a
-buildup [t] = t
-buildup ts  = buildup (pairing ts)
+defaultSettings :: Settings ByteString SHA256
+defaultSettings = Settings {
+    hash0 =  MerkleTreeHash $ sha256 ""
+  , hash1 = \x -> MerkleTreeHash $ sha256 (BS.singleton 0x00 `BS.append` x)
+  , hash2 = \x y ->  MerkleTreeHash $ sha256 $ BS.concat [BS.singleton 0x01, BA.convert x, BA.convert y]
+  }
 
-pairing :: [Tree a] -> [Tree a]
-pairing (t:u:vs) = link t u : pairing vs
-pairing ts       = ts
+data HashTree inp ha =
+    Leaf !Int !(MerkleTreeHash ha) inp
+  | Node !Int Int !(MerkleTreeHash ha) !(HashTree inp ha) !(HashTree inp ha)
+  deriving (Eq, Show)
+
+singleton :: (ByteArrayAccess inp, HashAlgorithm ha)
+          => Settings inp ha -> inp -> Int -> HashTree inp ha
+singleton settings x i = Leaf i (hash1 settings x) x
+
+mth :: HashTree inp ha -> MerkleTreeHash ha
+mth (Leaf _ ha _)     = ha
+mth (Node _ _ ha _ _) = ha
+
+link :: (ByteArrayAccess inp, HashAlgorithm ha)
+     => Settings inp ha -> HashTree inp ha -> HashTree inp ha -> HashTree inp ha
+link settings l r = Node (idxl l) (idxr r) h l r
+  where
+    h = hash2 settings (mth l) (mth r)
+    idxl (Leaf i _ _) = i
+    idxl (Node i _ _ _ _) = i
+    idxr (Leaf i _ _) = i
+    idxr (Node _ i _ _ _) = i
+
+fromList :: (ByteArrayAccess inp, HashAlgorithm ha)
+         => Settings inp ha -> [inp] -> HashTree inp ha
+fromList _ [] = error "No Element"
+fromList settings xs = buildup settings $ map leaf $ zip xs [0..]
+  where
+    leaf = uncurry (singleton settings)
+
+buildup :: (ByteArrayAccess inp, HashAlgorithm ha)
+         => Settings inp ha -> [HashTree inp ha] -> HashTree inp ha
+buildup _        [t] = t
+buildup settings ts  = buildup settings (pairing settings ts)
+
+pairing :: (ByteArrayAccess inp, HashAlgorithm ha)
+         => Settings inp ha -> [HashTree inp ha] -> [HashTree inp ha]
+pairing settings (t:u:vs) = link settings t u : pairing settings vs
+pairing _        ts       = ts
 
 ----------------------------------------------------------------
 {-
